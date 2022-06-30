ec57d94d713d363cbda5a667a05c114096d864d1
akzhu <aizhu71301@gmail.com>
2022-06-30 12:00:05 -0700

    About Me

---
+++ Getting-Started-with-Python-AidanZhu1/about_me.py
@@ -0,0 +1,17 @@
+full_name = "Aidan Zhu"
+nickname = "nadia"
+age = 20
+python_experience = True
+
+hobbies = ["watching movies", "playing basketball", "watching tv shows", "bowling", "playing golf"]
+
+favorites = {"movie": "Crazy Rich Asians", "food": "breakfast burritos", "hobby": hobbies[1], "game": "NBA 2k"}
+
+print(full_name, nickname, age, python_experience)
+print()
+print(hobbies)
+print()
+print(favorites)
+
+
+all_vars = dict(vars())
